b849cb6a1124ce20904a64da6f34e0905df01792
FathyV <hung.tovi@gmail.com>
2016-08-31 00:30:26 +0700

    Update Supplier View Model
    Create the SupplierRevenue class which used to store the revenue of the
    products.

---
+++ ProjectAlibaba/src/WebApplication_AlibabaShop/ViewModels/SupplierRevenue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_AlibabaShop.ViewModels
+{
+    public class SupplierRevenue
+    {
+        public int number { get; set; }
+        public string productName { get; set; }
+        public int totalOrder { get; set; }
+        public int totalEarn { get; set; }
+
+    }
+}
