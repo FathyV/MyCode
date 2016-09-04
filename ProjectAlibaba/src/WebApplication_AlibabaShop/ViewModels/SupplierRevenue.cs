a371df3cc195620855ae9bdba07243f9e541291a
FathyV <hung.tovi@gmail.com>
2016-09-04 22:31:40 +0700

    Index Page Update
    Finished the product report. Sorting the report will come in the future.

--- ProjectAlibaba/src/WebApplication_AlibabaShop/ViewModels/SupplierRevenue.cs
+++ ProjectAlibaba/src/WebApplication_AlibabaShop/ViewModels/SupplierRevenue.cs
@@ -9,6 +9,7 @@ namespace WebApplication_AlibabaShop.ViewModels
     {
         public string productName { get; set; }
         public int totalOrder { get; set; }
+        public int totalQuotation { get; set; }
         public decimal totalEarn { get; set; }
     }
 }
